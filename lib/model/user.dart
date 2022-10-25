54ad8055e9fd03165fbb1bd3cafc187a17950ed7
Justin W <justinw@archlinux.localdomain>
2022-10-25 13:39:34 +0100

    Delete result enum, fix user model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,20 +1,19 @@
 import 'dart:convert';
 
 class User {
-
-  String username;
-  String clientId;
+  String? username;
+  String? clientId;
   String? password;
+  String mailId;
 
-  User(this.username, this.clientId, {this.password});
+  User(this.mailId, {this.username, this.clientId, this.password});
 
-  factory User.fromJson(String json){
+  factory User.fromJson(String json) {
     Map jsonData = jsonDecode(json);
-    return User(jsonData["username"], jsonData["client_id"]);
+    return User(jsonData["mail_id"], username: jsonData["username"],clientId: jsonData["client_id"]);
   }
 
-  String toJson(){
-    return '{ "username": "$username", "client_id": "$clientId","password" : "$password"}';
+  String toJson(String event) {
+    return '{ "event": "$event", "username": "$username", "client_id": "$clientId","password" : "$password", "mail_id": "$mailId"}';
   }
-
 }
